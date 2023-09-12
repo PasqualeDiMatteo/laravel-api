26cfd71f172adb9cbae91576a8cdb84b22c5744e
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-09-12 14:20:36 +0200

    added show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -37,6 +37,9 @@ class ProjectController extends Controller
     public function show(string $id)
     {
         //
+        $project = Project::with("type", "technologies")->find($id);
+        if (!$project) return response(null, 404);
+        return response()->json($project);
     }
 
     /**
